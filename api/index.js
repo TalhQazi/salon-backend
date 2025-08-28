c37a04023bbc96a6c09dcc4aaf2ff0b9ada3caac
Peerzada-Zain-Nizami <qazizainulabaidin@gmail.com>
2025-08-29 01:16:41 +0500

    add function in index file

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
 const serverless = require("serverless-http");
 const app = require("../app");
 
+// ✅ Sirf development mode me .env load karo
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 module.exports = serverless(app);
