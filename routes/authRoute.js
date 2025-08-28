a212419b6cc78e622c1dac80d418a5d4a4b4acfa
Peerzada-Zain-Nizami <qazizainulabaidin@gmail.com>
2025-08-28 20:12:53 +0500

    phir say

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -9,14 +9,7 @@ const path = require("path");
 
 // Note: No need for uploads directory in serverless environment
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadsDir);
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({
   storage: storage,
