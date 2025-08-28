110dd0dd41bea0b0b175f78440ae0ed4d536f3b9
Peerzada-Zain-Nizami <qazizainulabaidin@gmail.com>
2025-08-28 16:05:55 +0500

    vercel changings

--- api/index.js
+++ api/index.js
@@ -1,10 +1,6 @@
 const serverless = require("serverless-http");
 const app = require("../app");
 
-<<<<<<< HEAD
 module.exports = serverless(app);
 
 
-=======
-module.exports.handler = serverless(app);
->>>>>>> master
